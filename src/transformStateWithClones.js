a012e7bb603ca26f68e27222c9c71c05829c9d65
jessika_morais <jessikanmorais@gmail.com>
2025-06-27 20:53:16 -0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,28 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // Faz uma cópia inicial para garantir que o objeto original não será alterado
+  const cloneObject = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(cloneObject, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete cloneObject[key];
+      }
+    } else if (action.type === 'clear') {
+      for (const key in cloneObject) {
+        delete cloneObject[key];
+      }
+    }
+
+    // Salva uma cópia do estado atual após cada ação
+    result.push({ ...cloneObject });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
